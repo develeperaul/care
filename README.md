486e12190520c2467b064818940d4ae90bef072a
Ralfik <developeraul@gmail.com>
2023-12-22 08:19:25 +0500

    feat: Добавлен header, sidebar , footer

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # care
 
 npx tailwindcss -i ./src/scss/main.scss -o ./src/scss/style.scss --watch
+
+z-index
+10 000
--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,23 @@
-import "../scss/style.scss";
-import "../index.pug";
+import '../scss/style.scss';
+import '../index.pug';
+
+import '../sections/header/index';
+import '../sections/footer/index';
+
+const btns = document.querySelectorAll('[data-action]');
+[...btns].forEach((btn) => {
+  btn.onclick = clickBtn;
+});
+function clickBtn(e) {
+  const target = this.getAttribute('data-target');
+  const action = this.getAttribute('data-action');
+  if (target) {
+    const el = document.querySelector(`#${target}`);
+    if (el) {
+      if (action === 'open') el.classList.add('active');
+      if (action === 'close') el.classList.remove('active');
+    }
+  }
+  console.log(e);
+  console.log(this);
+}
